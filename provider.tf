580d4024e4f610b79533bf7eab37ba5ecf9abdc4
SidFallenAngel <sid.edge94@gmail.com>
2017-02-01 15:27:28 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+        access_key = "${AKIAJDH5ILUN2QLWM5CQ}"
+        secret_key = "${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}"
+        region = "${var.region}"
+ }
+
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "region" {
+        default = "ap-northeast-1"
+}
+variable "availability_zone" {
+        default = "ap-northeast-1a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-5dd8b73a"
+}
+
+variable "key_name" {
+        default = "Tokyo_EC2"
+}
+
+variable "aws_access_key" {
+        default = "AKIAJDH5ILUN2QLWM5CQ"
+}
+
+variable "aws_secret_key" {
+        default = "CLKYceVNB4tWIUG7HJhNYXTA0pMyzvGCgIIQtuPz"
+}
+
